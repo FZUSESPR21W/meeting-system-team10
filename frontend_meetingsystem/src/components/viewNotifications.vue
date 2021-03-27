065174aaca051a1f507d09b0295227b2fbe0d154
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-03-27 16:55:20 +0800

    更新发布消息组件

--- frontend_meetingsystem/src/components/viewNotifications.vue
+++ frontend_meetingsystem/src/components/viewNotifications.vue
@@ -1,13 +1,78 @@
 <template>
-  <div id="viewNotifications"></div>
+  <div id="viewNotifications">
+    <Table :columns="columns" :data="data"></Table>
+    <Button class="button" @click="add">添加</Button>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'viewNotifications.vue'
+  name: 'viewNotifications.vue',
+  data () {
+    return {
+      columns: [
+        {
+          title: '发布者',
+          key: 'publisher'
+        },
+        {
+          title: '消息',
+          key: 'notification',
+          render: (h, { row, index }) => {
+            return h('Input', {
+              props: {
+                value: row.notification
+              },
+              on: {
+                'on-blur': (event) => {
+                  this.data[index].notification = event.target.value
+                  console.log(event.target.value)
+                }
+              }
+            })
+          }
+
+        }
+
+      ],
+      data: [
+        {
+          publisher: '铧钦',
+          notification: '牛逼'
+        }, {
+          publisher: '铧钦',
+          notification: '牛逼'
+        }, {
+          publisher: '铧钦',
+          notification: '牛逼'
+        }, {
+          publisher: '铧钦',
+          notification: '牛逼'
+        }
+
+      ]
+
+    }
+  },
+  methods: {
+    add () {
+      const addData = {
+        publisher: '',
+        notification: ''
+      }
+      this.data.push(addData)
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
+.table {
+  text-align: left;
+}
+.button {
+  margin-bottom: 20px;
+}
 
 </style>
