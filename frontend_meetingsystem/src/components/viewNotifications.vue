88b629c008b36657582373bff61da3e0a376a223
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-03-27 20:11:11 +0800

    给界面添加背景

--- frontend_meetingsystem/src/components/viewNotifications.vue
+++ frontend_meetingsystem/src/components/viewNotifications.vue
@@ -33,9 +33,7 @@ export default {
               }
             })
           }
-
         }
-
       ],
       data: [
         {
--- frontend_meetingsystem/src/views/index/chairmanIndex.vue
+++ frontend_meetingsystem/src/views/index/chairmanIndex.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="chairmanIndex">
-    <chart/>
+    <chart id="chairmanCss"/>
+    <div id="background"></div>
   </div>
+
 </template>
 
 <script>
@@ -22,5 +24,25 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+#chairmanCss{
+  width: 50%;
+  margin-top: 10%;
+  margin-left: 25%;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 5px 20px rgba(25, 25, 25, .75);
+  padding: 40px;
+}
+#background {
+  position: fixed;
+  background-image: url("../../assets/login_background.jpg");
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  z-index: -2;
+}
 </style>
