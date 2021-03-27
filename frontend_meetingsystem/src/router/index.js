01ac3896d8b589c096295c0735ebbc09e962d01e
林沧海 <1067407157@qq.com>
2021-03-27 11:45:24 +0800

    用户登录注册框架

--- frontend_meetingsystem/src/router/index.js
+++ frontend_meetingsystem/src/router/index.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 用户登录注册界面
+import Index from '../views/index.vue'
+import Login from '../components/index/login.vue'
+import Register from '../components/index/register'
+
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    name: 'index',
+    component: Index,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: Login
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: Register
+      }
+    ]
   }
 ]
 
