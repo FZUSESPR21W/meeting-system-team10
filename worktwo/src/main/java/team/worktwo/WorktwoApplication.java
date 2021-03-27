0835e8d128f47bff43713f3f470247aa8a65f23a
盫曌 <48637279+1069042059@users.noreply.github.com>
2021-03-26 22:12:21 -0500

    框架初始化

---
+++ worktwo/src/main/java/team/worktwo/WorktwoApplication.java
@@ -0,0 +1,13 @@
+package team.worktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorktwoApplication.class, args);
+    }
+
+}
---
+++ worktwo/src/test/java/team/worktwo/WorktwoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.worktwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
