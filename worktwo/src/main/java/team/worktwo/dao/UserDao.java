5acd19ecb8987e93aa7b497a0e7f43db6da76e0f
zwh1426 <1515071283@qq.com>
2021-03-27 15:46:40 +0800

    更新UserDao

--- worktwo/src/main/java/team/worktwo/dao/UserDao.java
+++ worktwo/src/main/java/team/worktwo/dao/UserDao.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 import team.worktwo.pojo.User;
 
 public interface UserDao {
-    @Select("SELECT * FROM user where account=#{account},password=#{password}")
-    List<User> getUserList();
+    @Select("SELECT * FROM user where account=#{account} and password=#{password}")
+    List<User> getUserList(User user);
 }
