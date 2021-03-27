8a0d262da7d1ac3ba71e5499d5a7b4b7c1afdd0b
cj-whales <842111302@qq.com>
2021-03-27 19:20:43 +0800

    新增语言包

--- frontend_meetingsystem/src/assets/lang/de_DE.js
+++ frontend_meetingsystem/src/assets/lang/de_DE.js
@@ -1,1 +1,34 @@
 // 德语
+module.exports = {
+
+  language: {
+    name: '中文'
+  },
+
+  index: {
+    login: 'Einloggen',
+    register: 'registrieren'
+  },
+  login: {
+    holder1: 'Konto',
+    holder2: 'Passwort',
+    mes1: 'Bitte geben Sie die Kontonummer ein',
+    mes2: 'Bitte geben Sie das Passwort ein',
+    mes3: 'Passwort darf nicht kleiner als sechs Ziffern sein',
+    mes4: 'Kontonummer ist größer als sechs'
+  },
+  register: {
+    holder1: 'Konto',
+    holder2: 'Kontaktinformationen',
+    holder3: 'Passwort',
+    holder4: 'Passwort bestätigen',
+    btn: 'register',
+    error1: 'Bitte geben Sie das Passwort ein!!',
+    error2: 'Das Passwort darf nicht kleiner als 6 Stellen sein!!',
+    error3: 'Bitte geben Sie das Passwort erneut ein!!',
+    error4: 'Die beiden Passworteinträge sind inkonsistent !!!!',
+    error5: 'Kontaktinformationen dürfen nicht leer sein!!',
+    error6: 'Konto darf nicht leer sein!!',
+    error7: 'Kontonummer darf nicht kleiner als 6 Ziffern sein!!'
+  }
+}
